287c7a2d22bc64fe4f85a7644f1f3c19beeaa1ae
ahmed_rabie_eg <eizomizo12@gmail.com>
2026-08-12 20:55:27 +0300

    Initial code skeleton

---
+++ src/Vec2.cpp
@@ -0,0 +1,13 @@
+#include "Vec2.h"
+#include <math.h>
+
+Vec2::Vec2()
+{
+}
+
+Vec2::Vec2(float xin, float yin)
+    : x(xin), y(yin)
+{
+}
+
+// TODO: Implement the remaining Vec2 functions
--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,9 @@
-#include "imconfig-SFML.h"
-#include "imgui-SFML.h"
-#include "imgui.h"
-#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
-#include <SFML/System/Clock.hpp>
-#include <iostream>
+#include "Game.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({(unsigned int) 1280, (unsigned int) 600}), "SFML Works!");
-    window.setFramerateLimit(60);
-    if (!ImGui::SFML::Init(window))
-    {
-        return -1;
-    }
-    sf ::Clock deltaClock;
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            ImGui::SFML::ProcessEvent(window, *event);
-
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-        }
-
-        // Update ImGui
-        ImGui::SFML::Update(window, deltaClock.restart());
-        ImGui::SFML::Render(window);
-        window.display();
-    }
+    Game g("config.txt");
+    g.run();
+    return 0;
 }
