ea15762103a057d606abaa439b1967c3dd5ebf14
kengit1 <es.akram.saad2023@alexu.edu.eg>
2026-08-12 15:49:59 +0300

    Modified the code correctly + the clang formatter for consistant allman style

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,31 @@
 #include "imconfig-SFML.h"
+#include "imgui-SFML.h"
 #include "imgui.h"
-#include <iostream>
-#include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode({(unsigned int)1280, (unsigned int)600}), "SFML Works!");
-    window.setFramerateLimit(60);
+#include <SFML/Graphics.hpp>
+#include <SFML/System/Clock.hpp>
+#include <iostream>
+
+int main() {
+  sf::RenderWindow window(
+      sf::VideoMode({(unsigned int)1280, (unsigned int)600}), "SFML Works!");
+  window.setFramerateLimit(60);
+  if (!ImGui::SFML::Init(window)) {
+    return -1;
+  }
+  sf ::Clock deltaClock;
+  while (window.isOpen()) {
+    while (const std::optional event = window.pollEvent()) {
+      ImGui::SFML::ProcessEvent(window, *event);
+
+      if (event->is<sf::Event::Closed>()) {
+        window.close();
+      }
+    }
+
+    // Update ImGui
+    ImGui::SFML::Update(window, deltaClock.restart());
+    ImGui::SFML::Render(window);
+    window.display();
+  }
 }
